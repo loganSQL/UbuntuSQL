7befc2630a2b4e4a7cfe6df76a01f635a21b5be9
logansql <loganchen@hotmail.com>
2018-01-04 14:31:36 -0500

    rename to 01_install.sh

---
+++ 01_github/01_install.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+# A Installation Bash script for github on Ubuntu
+# https://www.howtoforge.com/tutorial/install-git-and-github-on-ubuntu-14.04/
+sudo apt-get install git
+git config --global user.name "logansql"
+git config --global user.email "XXX@youremail.com"
+
+git config --list
+git config user.name
+
+git help config
+man git config
+git add -h
+
+# create a new repository
+pwd
+git init UbuntuSQL
+cd UbuntuSQL
+gedit README &
+gedit sample.c &
+git add README 
+git add sample.ca
+git add sample.c
+git commit -m "Test git on Ubuntu"
+git remote add origin https://github.com/loganSQL/UbuntuSQL.git
+git push origin master
+
+
